b0309cc6eb4b2c760e361b319590e6bf39d6150f
OscarAlberto <s245739@studenti.polito.it>
2020-04-09 23:03:45 +0200

    Parte 2

--- src/main/java/it/polito/tdp/corsi/EntryPoint.java
+++ src/main/java/it/polito/tdp/corsi/EntryPoint.java
@@ -1,32 +1,36 @@
 package it.polito.tdp.corsi;
 
+import it.polito.tdp.corsi.model.Model;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-        
-        stage.setTitle("JavaFX and Maven");
+
+        Model model = new Model();
+
+        FXMLController controller = loader.getController();
+
+        controller.setModel(model);
+
+        stage.setTitle("Gestore Corsi");
         stage.setScene(scene);
         stage.show();
     }
 
     /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
+     * The main() method is ignored in correctly deployed JavaFX application. main()
+     * serves only as fallback in case the application can not be launched through
+     * deployment artifacts, e.g., in IDEs with limited FX support. NetBeans ignores
+     * main().
      *
      * @param args the command line arguments
      */
